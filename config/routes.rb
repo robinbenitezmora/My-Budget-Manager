0be19b317777ca07b2aba8752b9555bb220c95ce
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-03 16:28:54 -0500

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  get 'clusters/index'
-  get 'clusters/show'
-  get 'clusters/new'
-  get 'clusters/edit'
-  get 'clusters/create'
-  get 'clusters/update'
-  get 'clusters/destroy'
-  get 'starts/index'
-  get 'starts/show'
-  get 'starts/new'
-  get 'starts/edit'
-  get 'starts/create'
-  get 'starts/update'
-  get 'starts/destroy'
+  resources :clusters
+  resources :users
+  resources :starts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
