c4df1f6f84630c2e4a373ca7a6655c0d939575c4
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-03 12:40:05 -0500

    Create controllers without relationships between them

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'starts/index'
+  get 'starts/show'
+  get 'starts/new'
+  get 'starts/edit'
+  get 'starts/create'
+  get 'starts/update'
+  get 'starts/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
