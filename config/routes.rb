c75b7b43fe904cfda50f5cf5929039e939306948
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-03 13:20:28 -0500

    Create controllers for the users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   get 'clusters/index'
   get 'clusters/show'
   get 'clusters/new'
