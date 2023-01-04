5ae34899590d28c0762692dddafbb0cad948f235
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-04 16:38:55 -0500

    Add validation in views

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord # rubocop:todo Layout/EndOfLine
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
