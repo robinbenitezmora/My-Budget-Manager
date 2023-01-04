b88496fc28e28eb99f2ddb938d370234e30a1d60
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-04 14:56:35 -0500

    Modify migration '20230104033007'

--- spec/views/associations/show.html.erb_spec.rb
+++ spec/views/associations/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper' # rubocop:todo Layout/EndOfLine
 
 RSpec.describe 'associations/show', type: :view do
   before(:each) do
-    assign(:association, Association.create!)
+    assign(:association, Association.create!())
   end
 
   it 'renders attributes in <p>' do
