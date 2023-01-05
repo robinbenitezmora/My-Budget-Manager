f9bb9cd8b8a33246e011ba3827ce133528ef1a4f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-05 14:50:43 -0500

    Add validations in the model files

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -2,4 +2,6 @@ class Cluster < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :user_id
   has_many :associations, foreign_key: :cluster_id, dependent: :destroy
   has_many :starts, through: :associations, foreign_key: :cluster_id, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/start.rb
+++ app/models/start.rb
@@ -2,4 +2,6 @@ class Start < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :author_id
   has_many :associations, foreign_key: :start_id, dependent: :destroy
   has_many :clusters, through: :associations, foreign_key: :start_id
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
