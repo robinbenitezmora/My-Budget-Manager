efbf776b456f48cb8562fcef6ca2219388a06049
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-05 14:40:02 -0500

    Update controller file 'asosciations_controller.rb'

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,8 +1,6 @@
 class AssociationsController < ApplicationController
 
-  # GET /associations or /associations.json
   def index
-    @associations = Association.all
     @cluster = Cluster.find(params[:cluster_id])
     @starts = @cluster.associations.includes(:start).map(&:start)
   end
