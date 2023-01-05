f1dec279593fe8737fe8c3983c43cccca57b2fdd
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-05 10:22:53 -0500

    Modify relations among tables in the models

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,5 +1,5 @@
 class Cluster < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :user_id
-  has_many :associations
-  has_many :starts, through: :associations
+  has_many :associations, foreign_key: :cluster_id, dependent: :destroy
+  has_many :starts, through: :associations, foreign_key: :cluster_id, dependent: :destroy
 end
--- app/models/start.rb
+++ app/models/start.rb
@@ -1,5 +1,5 @@
 class Start < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :author_id
-  has_many :associations
-  has_many :clusters, through: :associations
+  has_many :associations, foreign_key: :start_id, dependent: :destroy
+  has_many :clusters, through: :associations, foreign_key: :start_id
 end
