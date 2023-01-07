10d678f5198ddcc0af341877fd739a9bca96b5c3
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-07 16:39:33 -0500

    Udpate production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.serve_static_files = true
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
