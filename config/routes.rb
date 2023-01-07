786a09977ad5fe7817a3e0b0469a2c450c705681
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-07 15:58:28 -0500

    Add render in order to deploy the app

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
       resources :starts, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:show]
   # Defines the root path route ("/")
   root "clusters#index"
 end
